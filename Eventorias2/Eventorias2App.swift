2f78df35cbbca1643ca602477bf20a6eb651771b
alassane der <alassaneibrahimder@gmail.com>
2025-07-12 10:50:16 +0200

    Initial Commit

---
+++ Eventorias2/Eventorias2App.swift
@@ -0,0 +1,17 @@
+//
+//  Eventorias2App.swift
+//  Eventorias2
+//
+//  Created by Alassane Der on 12/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Eventorias2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
